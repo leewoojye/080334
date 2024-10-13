3e49de146fae9104419411baede0424096b65bdd
WooJe <whitehole36@naver.com>
2024-10-13 11:10:52 +0000

    three

--- ch9/9.5/nodebird/routes/page.js
+++ ch9/9.5/nodebird/routes/page.js
@@ -5,13 +5,16 @@ const { Post, User, Hashtag } = require('../models');
 const router = express.Router();
 
 router.use((req, res, next) => {
+  // 다음 미들웨어나 요청에서 접근할 수 있는 locals 변수
   res.locals.user = req.user;
   res.locals.followerCount = req.user ? req.user.Followers.length : 0;
   res.locals.followingCount = req.user ? req.user.Followings.length : 0;
   res.locals.followerIdList = req.user ? req.user.Followings.map(f => f.id) : [];
+  // next()가 있어야 다음 미들웨어로 넘어갈 수 있음.
   next();
 });
 
+// 라우터의 마지막 미들웨어는 컨트롤러라고 칭함 (isLoggedIn.. 등)
 router.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile', { title: '내 정보 - NodeBird' });
 });
@@ -35,6 +38,7 @@ router.get('/', async (req, res, next) => {
     });
   } catch (err) {
     console.error(err);
+    // next() 인자에 에러 객체가 들어가면 에러 핸들러가 다음 미들웨어로 실행됨.
     next(err);
   }
 });
